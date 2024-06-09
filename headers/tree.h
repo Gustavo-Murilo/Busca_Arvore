1ae5c2a51d4c815a311b068d6ff10c7e1fde1f43
Gustavo-Murilo <gustavomurilo012@gmail.com>
2024-06-09 10:44:26 -0300

    implentação de tree

--- headers/tree.h
+++ headers/tree.h
@@ -1,4 +1,18 @@
 #ifndef TREE_H
 #define TREE_H
 
+typedef struct Node
+{
+    int iPayload;
+    Node* ptrLeft = nullptr;
+    Node* ptrRight = nullptr;
+} Node;
+
+Node* createNode(int);
+Node* insertNode(Node*, int);
+void deleteTree(Node*);
+void deleteNode(Node*);
+Node* lesserLeaf(Node*);
+Node* higherLeaf(Node*);
+
 #endif // TREE_H
--- implementations/tree.cpp
+++ implementations/tree.cpp
@@ -1,2 +1,102 @@
 #include "../headers/tree.h"
 #include <iostream>
+
+Node* createNode(int iValue)
+{
+    Node* temp = (Node*) malloc(sizeof(Node));
+    
+    if (temp == nullptr)
+    {
+        std::cerr << "Erro em createNode: malloc" << std::endl;
+        exit(1);
+    }
+    
+    temp->iPayload = iValue;
+    temp->ptrLeft = nullptr;
+    temp->ptrRight = nullptr;
+    
+    return temp;
+}
+
+Node* insertNode(Node* startingNode, int iData)
+{
+    if(startingNode == nullptr)
+    {
+        return createNode(iData);
+    }
+    
+    if(iData < startingNode->iPayload)
+    {
+        startingNode->ptrLeft = insertNode(startingNode->ptrLeft, iData);
+    }
+    else
+    {
+        startingNode->ptrRight = insertNode(startingNode->ptrRight, iData);
+    }
+    
+    return startingNode;
+}
+
+void destroyTree(Node* startingNode)
+{
+    if(startingNode == nullptr)
+    {
+        return;
+    }
+    
+    destroyTree(startingNode->ptrLeft);
+    destroyTree(startingNode->ptrRight);
+    
+    free(startingNode);
+}
+
+Node* deleteNode(Node* startingNode, int iData)
+{
+    if (startingNode == nullptr) return nullptr;
+    
+    if (iData < startingNode->iPayload) startingNode->ptrLeft = deleteNode(startingNode->ptrLeft, iData);
+    else if (iData > startingNode->iPayload) startingNode->ptrRight = deleteNode(startingNode->ptrRight, iData);
+    else
+    {
+        Node* ptrTemp = nullptr;
+        
+        if (startingNode->ptrLeft == nullptr)
+        {
+            ptrTemp = startingNode->ptrRight;
+            free(startingNode);
+            return ptrTemp;
+        }
+        else if (startingNode->ptrRight == nullptr)
+        {
+            ptrTemp = startingNode->ptrLeft;
+            free(startingNode);
+            return ptrTemp;            
+        }
+        
+        ptrTemp = lesserLeaf(startingNode->ptrRight);
+        
+        startingNode->iPayload = ptrTemp->iPayload;
+        
+        startingNode->ptrRight = deleteNode(startingNode->ptrRight, ptrTemp->iPayload);
+    }
+    
+    return startingNode;
+}
+
+Node* lesserLeaf(Node* startingNode)
+{
+    Node* ptrCurrent = startingNode;
+ 
+    while (ptrCurrent && ptrCurrent->ptrLeft != nullptr) ptrCurrent = ptrCurrent->ptrLeft;
+    
+    return ptrCurrent;
+}
+
+Node* higherLeaf(Node* startingNode)
+{
+    Node* ptrCurrent = startingNode;
+ 
+    while (ptrCurrent && ptrCurrent->ptrRight != nullptr) ptrCurrent = ptrCurrent->ptrRight;
+    
+    return ptrCurrent;
+}
